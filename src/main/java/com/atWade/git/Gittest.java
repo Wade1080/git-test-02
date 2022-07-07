7d5d2bff1db22196d7dab81635d4e38a15a39260
Wade <Wade1080@163.com>
2022-07-07 17:37:10 +0800

    first commit

--- src/main/java/com/atWade/git/Gittest.java
+++ src/main/java/com/atWade/git/Gittest.java
@@ -3,7 +3,7 @@ package com.atWade.git;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("1111");
+        System.out.println("11111");
         System.out.println("1111");
     }
 }
