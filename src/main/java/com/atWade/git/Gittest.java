f8dfcc87197692b58a0e6e2f94b083d22f58cfe7
Wade <Wade1080@163.com>
2022-07-07 17:37:41 +0800

    second commit

--- src/main/java/com/atWade/git/Gittest.java
+++ src/main/java/com/atWade/git/Gittest.java
@@ -4,6 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("11111");
-        System.out.println("1111");
+        System.out.println("11112");
     }
 }
