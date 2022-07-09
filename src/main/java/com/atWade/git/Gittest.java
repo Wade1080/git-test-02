be923fc2c5c8d95b2e1afe045a78975e108dc6ec
Wade <Wade1080@163.com>
2022-07-09 09:31:12 +0800

    master-1

--- src/main/java/com/atWade/git/Gittest.java
+++ src/main/java/com/atWade/git/Gittest.java
@@ -1,11 +1,8 @@
 package com.atWade.git;
 
 public class Gittest {
-    public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("11111");
-        System.out.println("11113");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix2");
+    public static void main(String[] args) {    System.out.println("Git-1");
+        System.out.println("Git-1");
+        System.out.println("Git-2");
     }
 }
