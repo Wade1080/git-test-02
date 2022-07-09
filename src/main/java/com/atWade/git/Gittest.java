71b9bb22c874dfbc419e7681f1d3084909dbddda
Wade <Wade1080@163.com>
2022-07-09 09:31:42 +0800

    hot-fix-1

--- src/main/java/com/atWade/git/Gittest.java
+++ src/main/java/com/atWade/git/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {    System.out.println("Git-1");
         System.out.println("Git-1");
         System.out.println("Git-2");
+        System.out.println("Git-3");
     }
 }
